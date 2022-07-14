07bc70ee863d50c4099f4d4cdf7a6235a2325bd4
Khiroun <em_khiroun@esi.dz>
2022-07-14 17:30:10 +0100

    fix logo size and position in mobile mode

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -103,17 +103,14 @@ const Navbar = () => {
           mr: 2,
           display: { xs: "flex", md: "none" },
           flexGrow: 1,
-          fontFamily: "monospace",
-          fontWeight: 700,
-          letterSpacing: ".3rem",
-          color: "inherit",
           textDecoration: "none",
+          justifyContent: "center",
         }}
       >
         <img
           src="/logo1.jpg"
           style={{
-            maxWidth: "100%",
+            maxWidth: "30%",
           }}
         />
       </Typography>
